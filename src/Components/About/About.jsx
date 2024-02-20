7909823610b4f3ab3f8cea6d594ad8545fe4999c
GemboJones <gembojones@gmail.com>
2024-02-20 23:35:50 +0000

    updates About and Hero sections

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,25 +1,38 @@
 import React from "react";
-import styles from "../About/About.module.css";
+import styles from "./About.module.css";
 import { getImageUrl } from "../../utils";
 
 export const About = () => {
   return (
     <section className={styles.container}>
-      <h2>About</h2>
+      <h2 className={styles.title}>About</h2>
       <div className={styles.content}>
-        <img src={getImageUrl("hero/hero-image.png")} alt="headshot" />
-        <ul>
-          <li>
-            <h1 className={styles.title}>Front-End Dev</h1>
-            <p className={styles.description}>I'm a front-end dev</p>
+        <img
+          className={styles.aboutImage}
+          src={getImageUrl("hero/hero-image.png")}
+          alt="headshot"
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("nav/closeIcon.png")} alt="icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Front-End Dev</h3>
+              <p>I'm a front-end dev</p>
+            </div>
           </li>
-          <li>
-            <h1 className={styles.title}>Front-End Dev</h1>
-            <p className={styles.description}>I'm a front-end dev</p>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("nav/closeIcon.png")} alt="icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Front-End Dev</h3>
+              <p>I'm a front-end dev</p>
+            </div>
           </li>
-          <li>
-            <h1 className={styles.title}>Front-End Dev</h1>
-            <p className={styles.description}>I'm a front-end dev</p>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("nav/closeIcon.png")} alt="icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Front-End Dev</h3>
+              <p>I'm a front-end dev</p>
+            </div>
           </li>
         </ul>
       </div>
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from "../Hero/Hero.module.css"
+import styles from "./Hero.module.css"
 import { getImageUrl } from '../../utils';
 
 export const Hero = () => {
@@ -8,16 +8,22 @@ export const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Gemma 👋</h1>
         <p className={styles.description}>
-          I'm a highly collaborative developer who builds websites and apps with an emphasis on user experience and design.
+          I'm a highly collaborative developer who builds websites and apps with
+          an emphasis on user experience and design.
         </p>
-        <a
-          href="https://www.linkedin.com/in/gemma-randall-design/"
-          target="_blank"
-          rel="noopener"
-          className={styles.contactBtn}
-        >
-          Let's connect! ↗️
-        </a>
+        <div className={styles.btnContainer}>
+          <a href="#about" className={styles.primaryBtn}>
+            Latest work 🡓
+          </a>
+          <a
+            href="https://www.linkedin.com/in/gemma-randall-design/"
+            target="_blank"
+            rel="noopener"
+            className={styles.secondaryBtn}
+          >
+            Connect 🡕
+          </a>
+        </div>
       </div>
       <img
         className={styles.heroImg}
