d2d891d5c5c52787f1f0b6206a5940b8e42e309e
GemboJones <gembojones@gmail.com>
2024-02-23 23:54:32 +0000

    updates Skills component

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,13 +1,47 @@
 import React from "react";
-import skills from "../../data/tech.json";
+import styles from "./Skills.module.css";
+import tech from "../../data/tech.json";
+import { getImageUrl } from "../../utils";
+import history from "../../data/experience.json"
 
 export const Skills = () => {
   return (
-    <section id="skills">
-      <h2>Skills</h2>
-      <div>
-        <div></div>
-        <ul></ul>
+    <section className={styles.container} id="skills">
+      <h2 className={styles.title}>Skills</h2>
+      <div className={styles.content}>
+        <div className={styles.skills}>
+          {tech.map((tech, id) => {
+            return (
+              <div key={id} className={styles.skill}>
+                <div className={styles.skillImageContainer}>
+                  <img src={getImageUrl(tech.imageSrc)} alt={tech.title} />
+                </div>
+                <p>{tech.title}</p>
+              </div>
+            );
+          })}
+        </div>
+        <ul className={styles.history}>
+          {history.map((historyItem, id) => {
+            return (
+              <li key={id} className={styles.historyItem}>
+                <img
+                  src={getImageUrl(historyItem.imageSrc)}
+                  alt={`${historyItem.organisation} Logo`}
+                />
+                <div className={styles.historyItemDetails}>
+                  <h3>{`${historyItem.role}, ${historyItem.organisation}`}</h3>
+                  <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
+                  <ul>
+                    {historyItem.experiences.map((experience, id) => {
+                      return <li key={id}>{experience}</li>;
+                    })}
+                  </ul>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </section>
   );
