3cc39e93e0883f79bb97b274d602bff6ab13311c
GemboJones <gembojones@gmail.com>
2024-09-27 02:12:28 +0100

    adds icon attribution

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,33 +4,41 @@ import { getImageUrl } from "../../utils";
 
 export const Footer = () => {
   return (
-    <footer className={styles.container} id="contact">
-      <div className={styles.text}>
-        <h2>Contact</h2>
-        <p>Let's connect! 🔗</p>
+    <>
+      <footer className={styles.container} id="contact">
+        <div className={styles.text}>
+          <h2>Contact</h2>
+          <p>Let's connect! 🔗</p>
+        </div>
+        <ul className={styles.links}>
+          <li className={styles.link}>
+            <img src={getImageUrl("footer/githubIcon.png")} alt="icon" />
+            <a
+              href="https://github.com/GemboJones"
+              target="_blank"
+              rel="noopener"
+            >
+              github.com/GemboJones
+            </a>
+          </li>
+          <li className={styles.link}>
+            <img src={getImageUrl("footer/linkedinIcon.png")} alt="icon" />
+            <a
+              href="https://www.linkedin.com/in/gemma-randall-design"
+              target="_blank"
+              rel="noopener"
+            >
+              linkedin.com/in/gemma-randall-design
+            </a>
+          </li>
+        </ul>
+      </footer>
+
+      <div className={styles.attribution}>
+        <a target="_blank" href="https://www.flaticon.com/authors/freepik">
+          Icons created by Freepik - Flaticon
+        </a>
       </div>
-      <ul className={styles.links}>
-        <li className={styles.link}>
-          <img src={getImageUrl("footer/githubIcon.png")} alt="icon" />
-          <a
-            href="https://github.com/GemboJones"
-            target="_blank"
-            rel="noopener"
-          >
-            github.com/GemboJones
-          </a>
-        </li>
-        <li className={styles.link}>
-          <img src={getImageUrl("footer/linkedinIcon.png")} alt="icon" />
-          <a
-            href="https://www.linkedin.com/in/gemma-randall-design"
-            target="_blank"
-            rel="noopener"
-          >
-            linkedin.com/in/gemma-randall-design
-          </a>
-        </li>
-      </ul>
-    </footer>
+    </>
   );
 };
