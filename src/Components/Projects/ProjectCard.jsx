00805a374cd3d80613cc59fa63792a56c35be726
GemboJones <gembojones@gmail.com>
2025-02-03 01:28:29 +0000

    removed hardcoded button text

--- src/Components/Projects/ProjectCard.jsx
+++ src/Components/Projects/ProjectCard.jsx
@@ -2,36 +2,36 @@ import React from 'react'
 import styles from "./ProjectCard.module.css"
 import { getImageUrl } from "../../utils";
 
-export const ProjectCard = ({ project: {title, description, imageSrc, skills, demo, source} }) => {
+export const ProjectCard = ({ project: {title, description, imageSrc, skills, demo, demoText, source} }) => {
   return (
     <div className={styles.container}>
       <img src={getImageUrl(imageSrc)} alt={title} className={styles.image} />
       <div className={styles.contentContainer}>
-      <ul>
-        {skills.map((skill, id) => {
-          return <li key={id}>{skill}</li>;
-        })}
-      </ul>
-      <h3>{title}</h3>
-      <p>{description}</p>
-      <div className={styles.btnContainer}>
-        <a
-          href={demo}
-          target="_blank"
-          rel="noopener"
-          className={styles.primaryBtn}
-        >
-          Demo
-        </a>
-        <a
-          href={source}
-          target="_blank"
-          rel="noopener"
-          className={styles.secondaryBtn}
-        >
-          Source code
-        </a>
-      </div>
+        <ul>
+          {skills.map((skill, id) => {
+            return <li key={id}>{skill}</li>;
+          })}
+        </ul>
+        <h3>{title}</h3>
+        <p>{description}</p>
+        <div className={styles.btnContainer}>
+          <a
+            href={demo}
+            target="_blank"
+            rel="noopener"
+            className={styles.primaryBtn}
+          >
+            {demoText}
+          </a>
+          <a
+            href={source}
+            target="_blank"
+            rel="noopener"
+            className={styles.secondaryBtn}
+          >
+            Source code
+          </a>
+        </div>
       </div>
     </div>
   );
